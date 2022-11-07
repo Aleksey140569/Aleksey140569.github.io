632b7b909efa96f811d9ef0223a036e5b2a2eb41
Aleksey140569 <stranger140569@yandex.ru>
2022-11-07 18:29:39 +0300

    fix format

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 $(function() {
   $('.gamburger').click (function(e) {
     e.preventDefault();
-	  $(this).toggleClass('gamburger-active');
-	  $('.menu-nav').toggleClass('d-block').toggleClass('menu-active');
+    $(this).toggleClass('gamburger-active');
+    $('.menu-nav').toggleClass('d-block').toggleClass('menu-active');
   });
 
   $('.form-popup').click(function(e) {
-    console.log($('.form-popup'));
     e.preventDefault();
     $('#exampleModal').arcticmodal();
   });
